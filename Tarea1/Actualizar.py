2247a86c9a239adaa8de882c16fb7054c77fcb4a
Salvytore08 <salvatoremontenegro08@gmail.com>
2023-02-09 20:54:30 -0500

    [UPDATE]

--- Tarea1/Actualizar.py
+++ Tarea1/Actualizar.py
@@ -1,26 +1,33 @@
 import time
-
-def actualizar(
-    dict:dict
-):
-    """
-    Función que permite actualizar usuarios de la biblioteca
-    
-    ~Parámetros~
-     dict = Diccionario
-    """
-    nom = str(input('\nPor favor ingrese el nombre del usuario que desea acualizar: '))
-    if nom.lower() in dict:
-        b = str(input('¿Seguro que quiere actualizar el usuario? (Si o No): '))
-        if b.lower() == 'si':
-            nom = str(input('Por favor ingrese el nuevo nombre: '))
-            lib = str(input('Por favor ingrese el nuevo libro que desea pedir: '))
-            nom = nom.lower()
-            lib = lib.lower()
-            dict.update({nom:lib})
-        else:
-            print('Puede salir')
-            time.sleep(1.2)
-    else:
-        print('El usuario no está disponible')
-        time.sleep(1.2)
+try:
+    def actualizar(
+        dict:dict
+    ):
+        """
+        Función que permite actualizar usuarios de la biblioteca
+        
+        ~Parámetros~
+        dict = Diccionario
+        """
+        nom = str(input('\nPor favor ingrese el nombre del usuario que desea acualizar: '))
+        if nom.lower() in dict:
+            b = str(input('¿Seguro que quiere actualizar el usuario? (Si o No): '))
+            if b.lower() == 'si':
+                nom = str(input('Por favor ingrese el nuevo nombre: '))
+                lib = str(input('Por favor ingrese el nuevo libro que desea pedir: '))
+                nom = nom.lower()
+                lib = lib.lower()
+                dict.update({nom:lib})
+            else:
+                print('Puede salir')
+                time.sleep(1.2)
+   
+except ValueError:
+    print('Ingrese un nombre/opción/libro válidos')
+    print('Los nombres sólo pueden contener palabras')
+    print('Los libros sólo pueden contener palabras')
+    print('Las opciones sólo pueden ser si o no')
+    time.sleep(1.5)
+except KeyError:
+    print('El usuario no está disponible')
+    time.sleep(1.2)
--- Tarea1/Ver.py
+++ Tarea1/Ver.py
@@ -1,15 +1,20 @@
 import time
-
-def ver(
-    dict:dict
-):
-    """
-    Función que permite ver los usuarios de la biblioteca
-    
-    ~Parámetros~
-     dict = Diccionario
-    """
-    a = str(input('\nPor favor ingrese el usuario que desea ver: '))
-    b = a.lower()
-    print(f'{b} : {dict[b]}')
+try:
+    def ver(
+        dict:dict
+    ):
+        """
+        Función que permite ver los usuarios de la biblioteca
+        
+        ~Parámetros~
+        dict = Diccionario
+        """
+        a = str(input('\nPor favor ingrese el usuario que desea ver: '))
+        b = a.lower()
+        print(f'{b} : {dict[b]}')
+        time.sleep(1.2)
+        
+except ValueError:
+    print('Ingrese un nombre válidos')
+    print('Los nombres sólo pueden contener palabras')
     time.sleep(1.2)
