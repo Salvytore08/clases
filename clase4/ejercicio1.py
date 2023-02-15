ca7fdd4a4c989d281021dd56eb30884cd2d95854
Salvytore08 <salvatoremontenegro08@gmail.com>
2023-02-14 20:18:10 -0500

    [UPDATE]

--- clase4/ejercicio1.py
+++ clase4/ejercicio1.py
@@ -1,21 +1,25 @@
-lista = []
+lista1 = []
 
-def añadir(lista:list) -> list:
+def añadir(lista1:list) -> list:
     a = int(input('¿Cuántas palabras debe tener la lista?: '))
     for i in range(a):
         b = str(input('Por favor ingrese una palabra: '))
-        lista.append(b)
-    print(lista)
+        lista1.append(b)
+    return lista1
     
 def eliminar(lista:list) -> list:
+    lista2 = []
     a = int(input('¿Cuántas palabras desea eliminar?: '))
     for i in range(a):
         b = str(input('Por favor ingrese una palabra: '))
-        lista.remove(b)
+        lista2.append(b)
+    for i in lista2:
+        if i in lista:
+            lista.remove(i)
     print(lista)
     
-añadir(lista)
-eliminar(lista)
+print(añadir(lista1))
+eliminar(lista1)
 
 
 
